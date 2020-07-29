4230d078aad58942cc34c616d3540c73ec06185b
dowonhak <55188120+dowonhak@users.noreply.github.com>
2020-07-29 16:08:38 +0900

    Created calc.js

---
+++ calc.js
@@ -0,0 +1,15 @@
+function add(num1, num2) {
+  console.log(num1 + num2);
+}
+
+function subtract(num1, num2) {
+  // 이 부분을 채워주세요!
+}
+
+function multiply(num1, num2) {
+  console.log(num1 * num2);
+}
+
+function divide(num1, num2) {
+  console.log(num1 / num2);
+}
